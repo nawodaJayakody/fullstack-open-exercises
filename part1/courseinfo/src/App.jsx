83f8a5526cd4c0d99de14a1bf11f2fa21d34d411
Nawoda Jayakody <piumathnawoda@gmail.com>
2026-04-16 14:40:53 +0530

    updated exercise 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,21 +2,19 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  );
-};
-
 const Content = (props) => {
   return (
-    <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </div>
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
   );
 };
 
@@ -28,7 +26,6 @@ const Total = (props) => {
     </p>
   );
 };
-
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -49,6 +46,7 @@ const App = () => {
         part3={part3}
         exercises3={exercises3}
       />
+
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
